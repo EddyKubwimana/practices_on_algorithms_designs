82283d6d3d6c76cb55639f0788976eed1536d259
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-11-01 22:03:42 +0000

    mapping the provinces and communes

--- graph1.py
+++ graph1.py
@@ -2,6 +2,7 @@
 class commune:
     '''
     @class commune
+
     '''
 
     def __init__(self, name,center, population):
@@ -9,10 +10,31 @@ class commune:
         self.name = name
         self.population = population
         self.center = center
+        self.connected = []
 
     def __str__(self):
 
+
         return f" {self.name}"
+    
+    def addConnectedCommunes(self, commune):
+        ''' 
+        - Add a communes in the connected communes
+        
+        '''
+
+        if  not commune in self.connected:
+            self.connected.append(commune)
+
+
+    def getBorderingCommunes(self):
+        ''' 
+        - returns bordering communes
+
+        '''
+
+        return self.connected
+    
     
 
 
@@ -23,7 +45,8 @@ class province:
 
         self.name = name
         self.center = center
-        self.communes = {}
+        self.communes = []
+        self.connected =[]
 
     
     def __str__(self):
@@ -32,3 +55,41 @@ class province:
     
 
     def addCommune(self,commune):
+
+        '''
+        - Add a commune in the province
+        
+        '''
+
+        if commune in self.communes:
+            pass
+        else:
+            self.communes.append(commune)
+
+    
+    def addBorderingProvince(self,province):
+
+        '''
+        - Add bordering  province
+        
+        '''
+
+        if commune in self.connected:
+            pass
+        else:
+            self.connected.append(province)
+
+
+    def getBorderingProvince(self):
+
+        '''
+
+        - returns bordering provinces
+        
+        '''
+
+        return self.connected
+
+
+        
+        
