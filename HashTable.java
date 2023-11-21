1ab015ba89a44422be46bacb668e930bfd2b3c4a
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2023-11-21 12:14:57 +0000

    Added a clean documentation

--- HashTable.java
+++ HashTable.java
@@ -1,9 +1,41 @@
 
+/**
+ * Author: Eddy Kubwimana
+ * Email: eddy.kubwimana@ashesi.edu.gh
+ *
+ * Introduction:
+ *
+ * The HashTable class is part of the "DataStructuresLab developed Eddy . This lab is aimed at
+ * exploring and implementing  set of data structures implementations in Java. The HashTable class, specifically,
+ * offers a simple hash table using open addressing techniques, allowing for efficient insertion, search, and deletion
+ * of key-value pairs. The class employs a custom Entry class for representing individual entries in the hash table.
+ *
+ * Project Details:
+ *
+ * - Project Name: DataStructures lab
+ * - Author: Eddy Kubwimana
+ * - Email: eddy.kubwimana@ashesi.edu.gh
+ * - GitHub: https://github.com/EddyKubwimana/practices_on_algorithms_designs
+ *
+ * The HashTable class is designed to handle collision resolution using open addressing, specifically linear probing,
+ * in scenarios where there might be hash collisions. It includes methods for inserting new entries, searching for
+ * entries based on keys, and deleting entries by key. The project emphasizes clean and efficient implementations of
+ * various data structures to serve as educational resources or be integrated into larger software projects.
+ *
+ * Feel free to explore the project's GitHub repository for additional data structures and utility classes:
+ * https://github.com/EddyKubwimana/practices_on_algorithms_designs
+ *
+ * For questions or feedback, please contact John Doe at eddy.kubwimana@ashesi.edu.gh
+ */
+
+
+
 //=========================Entry class =======================
 /**
  * The Entry class represents a key-value pair used in data structures such as maps.
  * Each Entry object encapsulates an integer key and an integer value associated with that key.
  */
+
  class Entry {
 
 
@@ -24,9 +56,10 @@
      * @param key   The integer key for the Entry.
      * @param value The integer value associated with the key.
      */
-    public Entry(int key, int value) {
-        this.key = key;
-        this.value = value;
+
+    public Entry(int Id, int Grade) {
+        this.key = Id;
+        this.value = Grade;
     }
 
     /**
@@ -34,9 +67,50 @@
      *
      * @return A string in the format "{key : value}" representing the Entry.
      */
+
     public String toString() {
-        return "{" + this.key + " : " + this.value + "}";
+        return "{" + " Student ID : "+ this.key + " , Student Grade : " + this.value + "}";
     }
+
+
+   /**
+     * Gets the unique identifier (ID) associated with the student.
+     *
+     * @return The student's ID.
+     */
+
+    public int getId() {
+        return this.key;
+    }
+
+    /**
+     * Gets the grade associated with the student.
+     *
+     * @return The student's grade.
+     */
+
+    public int getGrade() {
+        return this.value;
+    }
+
+    /**
+     * Sets the unique identifier (ID) for the student.
+     *
+     * @param ID The new ID to be assigned to the student.
+     */
+    public void setId(int ID) {
+        this.key = ID;
+    }
+
+    /**
+     * Sets the grade for the student.
+     *
+     * @param Grade The new grade to be assigned to the student.
+     */
+    public void setGrade(int Grade) {
+        this.value = Grade;
+    }
+
 }
 
 
@@ -79,139 +153,136 @@ class Hash {
 
 
 //===================HashTable==================
-public class HashTable{
 
 
-    // This are instance variable the hasttable class
-    private Entry[]table;
+
+/**
+ * The HashTable class represents a simple hash table implementation using open addressing.
+ * It allows for inserting, searching, and deleting key-value pairs.
+ */
+public class HashTable {
+
+    /**
+     * An array to store entries (key-value pairs) in the hash table.
+     */
+    private Entry[] table;
+
+    /**
+     * The capacity of the hash table, representing the maximum number of entries it can hold.
+     */
     private int capacity;
+
+    /**
+     * The current number of entries in the hash table.
+     */
     private int length;
 
-
-    // constructor
-    // takes int capacity
-    public HashTable(int capacity){
-
+    /**
+     * Constructs a HashTable with the specified capacity.
+     *
+     * @param capacity The maximum number of entries the hash table can hold.
+     */
+    public HashTable(int capacity) {
         this.capacity = capacity;
         this.table = new Entry[this.capacity];
-
     }
 
-
-    public int getCapacity(){
-
+    /**
+     * Gets the capacity of the hash table.
+     *
+     * @return The maximum number of entries the hash table can hold.
+     */
+    public int getCapacity() {
         return this.capacity;
     }
 
-    public void insert(int key, int value){
-
-        if(this.length == capacity){
+    /**
+     * Inserts a key-value pair into the hash table.
+     * If the table is full, a message indicating that the table is full will be printed.
+     *
+     * @param key   The key of the entry to be inserted.
+     * @param value The value associated with the key.
+     */
+    public void insert(int key, int value) {
+        if (this.length == capacity) {
             System.out.println("=====The table is full ====");
-        }else{
-
-           int primaryIndex = new Hash().primaryHash(key,this.capacity);
-           int secondaryIndex = new Hash().secondaryHash(key);
-
-
-           int i = 0;
-
-           while (i<this.capacity){
-              
-            int index = (primaryIndex+(i*secondaryIndex))%this.capacity;
-
-            if (this.table[index]==null){
-                this.table[index] = new Entry(key,value);
-                this.length++;
-                return ;
-
-            }
-            else if ( this.table[index]!=null && this.table[index].key == key){
-
-                this.table[index].value = value;
-                return;
-
-                
-            }
-
-            else{
-
-                i++;
-
-            }
-
-
-            }
-
-           }
-
-        }
-
-
-        Entry retrieve(int key){
-
-            int primaryIndex = new Hash().primaryHash(key,this.capacity);
+        } else {
+            int primaryIndex = new Hash().primaryHash(key, this.capacity);
             int secondaryIndex = new Hash().secondaryHash(key);
 
-            int i= 0;
+            int i = 0;
 
-            while(i<this.capacity){
+            while (i < this.capacity) {
+                int index = (primaryIndex + (i * secondaryIndex)) % this.capacity;
 
-                int index = (primaryIndex+(i*secondaryIndex))%this.capacity;
-
-                if( this.table[index] != null && this.table[index].key ==key){
-
-
-                    return this.table[index];
-
-                }
-                else if(this.table[index]==null){
-
-                    return null;
-
-                }
-
-                else{
-
+                if (this.table[index] == null) {
+                    this.table[index] = new Entry(key, value);
+                    this.length++;
+                    return;
+                } else if (this.table[index] != null && this.table[index].key == key) {
+                    this.table[index].value = value;
+                    return;
+                } else {
                     i++;
                 }
-
-            
-
             }
+        }
+    }
 
-            return null;}
+    /**
+     * Searches for an entry with the specified key in the hash table.
+     *
+     * @param key The key to search for in the hash table.
+     * @return The Entry object associated with the key, or null if not found.
+     */
+    public Entry search(int key) {
+        int primaryIndex = new Hash().primaryHash(key, this.capacity);
+        int secondaryIndex = new Hash().secondaryHash(key);
 
+        int i = 0;
 
+        while (i < this.capacity) {
+            int index = (primaryIndex + (i * secondaryIndex)) % this.capacity;
 
-        
-        Boolean delete(int key){
-
-
-
-            int primaryIndex = new Hash().primaryHash(key,this.capacity);
-            int secondaryIndex = new Hash().secondaryHash(key);
-
-            int i= 0;
-
-            while(i<this.capacity){
-
-                int index = (primaryIndex+(i*secondaryIndex))%this.capacity;
-
-                if( this.table[index].key==key){
-
-                    this.table[index] = null;
-                    return true;
-
-                }
-
+            if (this.table[index] != null && this.table[index].key == key) {
+                return this.table[index];
+            } else if (this.table[index] == null) {
+                return null;
+            } else {
                 i++;
             }
-    
+        }
 
-            return false;}
+        return null;
+    }
 
+    /**
+     * Deletes an entry with the specified key from the hash table.
+     * If the entry is successfully deleted, the method returns true; otherwise, it returns false.
+     *
+     * @param key The key of the entry to be deleted.
+     * @return True if the entry is successfully deleted, false otherwise.
+     */
+    public boolean delete(int key) {
+        int primaryIndex = new Hash().primaryHash(key, this.capacity);
+        int secondaryIndex = new Hash().secondaryHash(key);
 
-    
+        int i = 0;
+
+        while (i < this.capacity) {
+            int index = (primaryIndex + (i * secondaryIndex)) % this.capacity;
+
+            if (this.table[index].key == key) {
+                this.capacity--;
+                this.table[index] = null;
+                return true;
+            }
+
+            i++;
+        }
+
+        return false;
+    }
 
     public static void main(String[]args){
 
@@ -225,16 +296,15 @@ public class HashTable{
         table.insert(67,100);
         table.insert(100,1);
 
-        System.out.println(table.retrieve(83092025));
-        System.out.println(table.retrieve(93077));
-        System.out.println(table.retrieve(22));
-        System.out.println(table.retrieve(3418));
-        System.out.println(table.retrieve(23));
-        System.out.println(table.retrieve(1200));
-        System.out.println(table.retrieve(67));
-
+        System.out.println(table.search(83092025));
+        System.out.println(table.search(93077));
+        System.out.println(table.search(22));
+        System.out.println(table.search(3418));
+        System.out.println(table.search(23));
+        System.out.println(table.search(1200));
+        System.out.println(table.search(67));
         table.delete(67);
-        System.out.println(table.retrieve(67));
+        System.out.println(table.search(67));
 
 
 
